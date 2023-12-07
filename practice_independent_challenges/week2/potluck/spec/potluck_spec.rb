c8a1204567e79347bd420c6080ed1b509067e199
Igor Magalhaes <magalhaess.igor@gmail.com>
2023-12-06 22:54:44 -0500

    Refactor: add more variables to before each do block

--- practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
+++ practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
@@ -5,6 +5,12 @@ require './lib/potluck'
 describe Potluck do
    before(:each) do
       @potluck = Potluck.new("7-13-18")
+      @couscous_salad = Dish.new("Couscous Salad", :appetizer)
+      @cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+      @summer_pizza = Dish.new("Summer Pizza", :appetizer)
+      @roast_pork = Dish.new("Roast Pork", :entre)
+      @candy_salad = Dish.new("Candy Salad", :dessert)
+      @bean_dip = Dish.new("Bean Dip", :appetizer)
    end
 
    describe 'initialize' do
@@ -22,12 +28,13 @@ describe Potluck do
    end
 
    describe '#add_dish' do
-      it 'add dishes to Potluck dishes' do
-         couscous_salad = Dish.new("Couscous Salad", :appetizer)
-         cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
-         @potluck.add_dish(couscous_salad)
-         @potluck.add_dish(cocktail_meatballs)
-         expect(@potluck.dishes).to eq [couscous_salad, cocktail_meatballs]
+      before(:each) do
+         it 'add dishes to Potluck dishes' do
+         @potluck.add_dish(@couscous_salad)
+         @potluck.add_dish(@cocktail_meatballs)
+         expect(@potluck.dishes).to eq [@couscous_salad, @cocktail_meatballs]
       end
    end
+
+
 end
