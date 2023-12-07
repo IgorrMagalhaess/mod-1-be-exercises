86afc889bde9a8784b6bf74872ea57805da29abb
Igor Magalhaes <magalhaess.igor@gmail.com>
2023-12-06 21:01:04 -0500

    Test: add attributes of Dish to initialize test

--- practice_independent_challenges/week2/potluck/spec/dish_spec.rb
+++ practice_independent_challenges/week2/potluck/spec/dish_spec.rb
@@ -8,4 +8,14 @@ describe Dish do
          expect(dish).to be_a Dish
       end
 
+      it 'has a name' do
+         dish = Dish.new("Couscous Salad", :appetizer)
+         expect(dish.name).to eq "Couscous Salad"
+      end
+
+      it 'has a category' do
+         dish = Dish.new("Couscous Salad", :appetizer)
+         expect(dish.category).to eq :appetizer
+      end
+   end
 end
