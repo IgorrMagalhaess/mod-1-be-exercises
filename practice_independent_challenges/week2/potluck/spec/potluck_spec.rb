7f1e86050d28d6c2f93455a472c4dbd55f7d19eb
Igor Magalhaes <magalhaess.igor@gmail.com>
2023-12-06 22:33:32 -0500

    Test: Create add_dish method test; Refactor: add before each do block to reduce amout of code

--- practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
+++ practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
@@ -3,21 +3,31 @@ require './lib/dish'
 require './lib/potluck'
 
 describe Potluck do
+   before(:each) do
+      @potluck = Potluck.new("7-13-18")
+   end
+
    describe 'initialize' do
       it 'is a Potluck' do
-         potluck = Potluck.new("7-13-18")
-         expect(potluck).to be_a Potluck
+         expect(@potluck).to be_a Potluck
       end
 
       it 'has a date' do
-         potluck = Potluck.new("7-13-18")
-         expect(potluck.date).to eq "7-13-18"
+         expect(@potluck.date).to eq "7-13-18"
       end
 
       it 'start with no dishes' do
-         potluck = Potluck.new("7-13-18")
-         expect(potluck.dishes).to eq []
+         expect(@potluck.dishes).to eq []
       end 
    end
 
+   describe '#add_dish' do
+      it 'add dishes to Potluck dishes' do
+         couscous_salad = Dish.new("Couscous Salad", :appetizer)
+         cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+         @potluck.add_dish(couscous_salad)
+         @potluck.add_dish(cocktail_meatballs)
+         expect(@potluck.dishes).to be eq [couscous_salad, cocktail_meatballs]
+      end
+   end
 end
