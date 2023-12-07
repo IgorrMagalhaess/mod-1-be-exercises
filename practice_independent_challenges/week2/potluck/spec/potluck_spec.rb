ed8204e783bdba66bb1e79ef133b7bd9d6d23251
Igor Magalhaes <magalhaess.igor@gmail.com>
2023-12-06 21:09:03 -0500

    Test: create more tests for initialize method for Potluck class

--- practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
+++ practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
@@ -8,6 +8,16 @@ describe Potluck do
          potluck = Potluck.new("7-13-18")
          expect(potluck).to be_a Potluck
       end
+
+      it 'has a date' do
+         potluck = Potluck.new("7-13-18")
+         expect(potluck.date).to eq "7-13-18"
+      end
+
+      it 'start with no dishes' do
+         potluck = Potluck.new("7-13-18")
+         expect(potluck.dishes).to eq []
+      end 
    end
 
 end
