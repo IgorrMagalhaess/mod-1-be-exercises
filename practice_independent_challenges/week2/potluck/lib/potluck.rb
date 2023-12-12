75811117e6fdc9acb4bf4013f23eb7a2b7be3738
Igor Magalhaes <magalhaess.igor@gmail.com>
2023-12-12 17:53:46 -0500

    Feat: add #menu method

--- practice_independent_challenges/week2/potluck/lib/potluck.rb
+++ practice_independent_challenges/week2/potluck/lib/potluck.rb
@@ -13,4 +13,14 @@ class Potluck
    def get_all_from_category(category)
       @dishes.select { |dish| dish.category == category }
    end
+
+   def menu
+      menu = {}
+      @dishes.each do |dish|
+         menu[dish.category] ||= []
+         menu[dish.category].push (dish.name)
+         menu[dish.category].sort!  
+      end
+      menu
+   end
 end
